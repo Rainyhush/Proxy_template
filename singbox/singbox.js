f6456f4898bfb1e89640694294f9549daf9eafc4
Rainyhush <kulael3@gmail.com>
2024-07-06 21:01:33 +0800

    update

--- singbox/singbox.js
+++ singbox/singbox.js
@@ -17,7 +17,7 @@ config.outbounds.push(...proxies)
 
 config.outbounds.map(i => {
   if (['Other'].includes(i.tag)) {
-    i.outbounds.push(...proxies.filter(p => !/香港|hk|HK|Hong Kong|浙江|徐州|广州|武汉|襄阳|鞍山|杭州|济南|台湾|tw|TW|TaiWan|日本|jp|JP|Japan|新加坡|狮城|sg|SG|Singapore|德国|de|DE|Germany|韩国|kr|KR|Korea|美国|us|US|America/i.test(p.tag))
+    i.outbounds.push(...proxies.filter(p => !/香港|hk|HK|Hong Kong|浙江|宁波|宿迁|广东|徐州|广州|武汉|襄阳|鞍山|杭州|济南|台湾|tw|TW|TaiWan|日本|jp|JP|Japan|新加坡|狮城|sg|SG|Singapore|德国|de|DE|Germany|韩国|kr|KR|Korea|美国|us|US|America/i.test(p.tag))
       .map(p => p.tag))
   }
   if (['HK'].includes(i.tag)) {
@@ -53,7 +53,7 @@ config.outbounds.map(i => {
       .map(p => p.tag))
   }
   if (['AUTO'].includes(i.tag)) {
-    i.outbounds.push(...proxies.filter(p => /美国|香港|日本|新加坡|台湾|韩国|德国|英国|阿根廷|泰国|伊拉克|俄罗斯|土耳其|芬兰|浙江|徐州|广州|鞍山|襄阳|武汉|杭州|济南/i.test(p.tag))
+    i.outbounds.push(...proxies.filter(p => /美国|香港|日本|新加坡|台湾|韩国|德国|英国|阿根廷|泰国|伊拉克|俄罗斯|土耳其|芬兰|浙江|宁波|宿迁|广东|徐州|广州|鞍山|襄阳|武汉|杭州|济南/i.test(p.tag))
       .map(p => p.tag))
   }
 })
