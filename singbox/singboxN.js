48a88d144436844f29fc75122cb8052743b7abf0
Rainyhush <kulael3@gmail.com>
2024-10-08 13:27:59 +0000

    deleted:    js.js 	modified:   singbox/singboxN.js 	modified:   singbox/singboxN.json

--- singbox/singboxN.js
+++ singbox/singboxN.js
@@ -17,23 +17,27 @@ config.outbounds.push(...proxies)
 
 config.outbounds.map(i => {
   if (['Other'].includes(i.tag)) {
-    i.outbounds.push(...proxies.filter(p => !/香港|hk|HK|Hong Kong|浙江|宁波|宿迁|广东|镇江|徐州|广州|武汉|襄阳|鞍山|杭州|济南|台湾|tw|TW|TaiWan|日本|jp|JP|Japan|新加坡|狮城|sg|SG|Singapore|韩国|kr|KR|Korea/i.test(p.tag))
+    i.outbounds.push(...proxies.filter(p => !/香港|hk|HK|Hong Kong|浙江|宁波|宿迁|广东|镇江|徐州|广州|武汉|襄阳|鞍山|杭州|济南|台湾|tw|TW|TaiWan|日本|jp|JP|Japan|新加坡|狮城|sg|SG|Singapore/i.test(p.tag))
       .map(p => p.tag))
   }
-  if (['HK and TW'].includes(i.tag)) {
+  if (['HongKong'].includes(i.tag)) {
     i.outbounds.push(...proxies.filter(p => /香港|台湾|tw|TW|TaiWan|hk|HK|Hong Kong/i.test(p.tag))
+      .map(p => p.tag))
+  }
+  if (['TaiWan'].includes(i.tag)) {
+    i.outbounds.push(...proxies.filter(p => /台湾|tw|TW|TaiWan/i.test(p.tag))
       .map(p => p.tag))
   }
   if (['CN'].includes(i.tag)) {
     i.outbounds.push(...proxies.filter(p => /浙江|徐州|广州|镇江|鞍山|襄阳|武汉|杭州|济南/i.test(p.tag))
       .map(p => p.tag))
   }
-  if (['JP and SG'].includes(i.tag)) {
-    i.outbounds.push(...proxies.filter(p => /日本|新加坡|狮城|sg|SG|Singapore|jp|JP|Japan/i.test(p.tag))
+  if (['Japan'].includes(i.tag)) {
+    i.outbounds.push(...proxies.filter(p => /日本|jp|JP|Japan/i.test(p.tag))
       .map(p => p.tag))
   }
-  if (['KR'].includes(i.tag)) {
-    i.outbounds.push(...proxies.filter(p => /韩国|kr|KR|Korea/i.test(p.tag))
+  if (['Singapore'].includes(i.tag)) {
+    i.outbounds.push(...proxies.filter(p => /新加坡|狮城|sg|SG|Singapore/i.test(p.tag))
       .map(p => p.tag))
   }
   if (['Aii node'].includes(i.tag)) {
