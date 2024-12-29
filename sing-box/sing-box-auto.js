27795bce61ef488913c9ac6c940953abe1849bdd
Rainyhush <kulael3@gmail.com>
2024-12-29 12:20:25 +0800

    ooo

--- sing-box/sing-box-auto.js
+++ sing-box/sing-box-auto.js
@@ -14,62 +14,69 @@ let proxies = await produceArtifact({
 config.outbounds.push(...proxies);
 
 config.outbounds.map((i) => {
-  if (["Other"].includes(i.tag)) {
+  if (["其它"].includes(i.tag)) {
     i.outbounds.push(
       ...proxies
         .filter(
           (p) =>
-            !/镇江|徐州|武汉|济南|香港|direct|HongKong|HK|台湾|Taiwan|TW|日本|Japan|JP|新加坡|Singapore|SG|美国|America|US|韩国|Korea|KR|英国|United Kingdom|UK/i.test(
+            !/镇江|徐州|武汉|济南|香港|direct|HongKong|HK|台湾|Taiwan|TW|日本|Japan|JP|新加坡|Singapore|SG|美国|America|US|韩国|Korea|KR|英国|United Kingdom|UK|德国|Germany|DE/i.test(
               p.tag,
             ),
         )
         .map((p) => p.tag),
     );
   }
-  if (["HongKong"].includes(i.tag)) {
+  if (["香港"].includes(i.tag)) {
     i.outbounds.push(
       ...proxies
         .filter((p) => /香港|HongKong|HK/i.test(p.tag))
         .map((p) => p.tag),
     );
   }
-  if (["Taiwan"].includes(i.tag)) {
+  if (["台湾"].includes(i.tag)) {
     i.outbounds.push(
       ...proxies.filter((p) => /台湾|Taiwan|TW/i.test(p.tag)).map((p) => p.tag),
     );
   }
-  if (["Japan"].includes(i.tag)) {
+  if (["日本"].includes(i.tag)) {
     i.outbounds.push(
       ...proxies.filter((p) => /日本|Japan|JP/i.test(p.tag)).map((p) => p.tag),
     );
   }
-  if (["Singapore"].includes(i.tag)) {
+  if (["新加坡"].includes(i.tag)) {
     i.outbounds.push(
       ...proxies
         .filter((p) => /新加坡|Singapore|SG/i.test(p.tag))
         .map((p) => p.tag),
     );
   }
-  if (["Korea"].includes(i.tag)) {
+  if (["韩国"].includes(i.tag)) {
     i.outbounds.push(
       ...proxies.filter((p) => /韩国|Korea|KR/i.test(p.tag)).map((p) => p.tag),
     );
   }
-  if (["America"].includes(i.tag)) {
+  if (["美国"].includes(i.tag)) {
     i.outbounds.push(
       ...proxies
         .filter((p) => /美国|America|US/i.test(p.tag))
         .map((p) => p.tag),
     );
   }
-  if (["UnitedKingdom"].includes(i.tag)) {
+  if (["英国"].includes(i.tag)) {
     i.outbounds.push(
       ...proxies
-        .filter((p) => /英国|UnitedKingdom|United Kingdom|UK/i.test(p.tag))
+        .filter((p) => /英国|UnitedKingdom|United Kingdom|UK|GB/i.test(p.tag))
         .map((p) => p.tag),
     );
   }
-  if (["ChinaR"].includes(i.tag)) {
+  if (["德国"].includes(i.tag)) {
+    i.outbounds.push(
+      ...proxies
+        .filter((p) => /德国|Germany|DE/i.test(p.tag))
+        .map((p) => p.tag),
+    );
+  }
+  if (["中国"].includes(i.tag)) {
     i.outbounds.push(
       ...proxies
         .filter((p) => /徐州|武汉|镇江|济南/i.test(p.tag))
